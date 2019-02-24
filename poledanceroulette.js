0921eb7353c7dae825a20a593711799fa259b371
Danybradner <daenara@bradner.net>
2019-02-24 18:50:28 +1100

    added new readme rule

--- poledanceroulette.js
+++ poledanceroulette.js
@@ -1,11 +1,4 @@
-/* I want to be able to create a game that will pick random moves,
-the goal is help students create combos in their own style,
-it's also to help freestyling.
-Things  I need to consider: 
-1. Level of student in pole.
-2. Add a challenge in emotion
-3. add a movement. Example: add a "twizzle if possible,""use arms", "use head" 
-4. create a modified variation of easy, medium, hard, expert mode */
+
 let name = 'Dany';
 console.log (`Hello ${name}`);
 //Enter a name
